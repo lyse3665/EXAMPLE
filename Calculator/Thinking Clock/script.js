9a221fa4c127fe7e42d4eb1ee5b127389d5ae1c4
lydia <lydiasebaziga@gmail.com>
2023-01-11 20:56:49 -0800

    all

---
+++ Calculator/Thinking Clock/script.js
@@ -0,0 +1,22 @@
+function getSecondsSinceStartOfDay() {
+    return new Date().getSeconds() + 
+      new Date().getMinutes() * 60 + 
+      new Date().getHours() * 3600;
+  }
+      
+  setInterval( function() {
+      var time = getSecondsSinceStartOfDay();
+      console.log(time);
+      // your code here
+    d = new Date(); //object of date()
+    hr = d.getHours();
+    min = d.getMinutes();
+    sec = d.getSeconds();
+    hr_rotation = 30 * hr + min / 2; //converting current time
+    min_rotation = 6 * min;
+    sec_rotation = 6 * sec;
+  
+    hour.style.transform = `rotate(${hr_rotation}deg)`;
+    minutes.style.transform = `rotate(${min_rotation}deg)`;
+    seconds.style.transform = `rotate(${sec_rotation}deg)`;
+}, 1000);
